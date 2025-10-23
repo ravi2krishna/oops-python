bbb741c4c5e512ec7dc2ecaad644de145260a9dc
ravi2krishna <ravi2krishna@gmail.com>
2025-10-23 11:43:34 +0530

    basic oops fix with local variables

--- lms-1/student.py
+++ lms-1/student.py
@@ -6,7 +6,7 @@ class Student:
     
     # Constructor with Non-static / Instance variables specific to each student
     # Student ID, Name, Age, Email & Mobile Number
-    def __init__(self, student_id=None, student_name=None, student_age=None, student_email=None, student_mobile_number=None):
+    def __init__(self, student_id, student_name, student_age, student_email, student_mobile_number):
         # Instance variables specific to each student
         self.student_id = student_id
         self.student_name = student_name
@@ -14,13 +14,12 @@ class Student:
         self.student_email = student_email
         self.student_mobile_number = student_mobile_number
         
-        self.total_sessions_attended = 0
-        self.attendance_credits = 0
-        # NEWLY UPDATED
-        self.performance_credit_points = 0
-        self.final_credits = 0
-        self.trainer_rating = 0
-        
+    # Class method : display institute info 
+    @classmethod
+    def institute_info(cls):
+        print("="*50)
+        print("Welcome To "+Student.institute_name)
+        print("="*50)
         
     # Instance method: display basic student info => Hover Functionality 
     def student_basic_info(self):
@@ -38,42 +37,46 @@ class Student:
         print(f"Student Mobile No: {self.student_mobile_number}")        
 
     # Calculate sessions/attendance credits 
-    def sessions_credits(self):
+    def sessions_credits_cal(self):
+        total_sessions_attended = 0
+        attendance_credits = 0
         total_sessions_attended = int(input("Enter Total Sessions Attended: "))
-        self.total_sessions_attended = total_sessions_attended
         if total_sessions_attended >= 30:
-            self.attendance_credits += 5
+            attendance_credits = 5
         elif total_sessions_attended >= 20:
-            self.attendance_credits += 3
+            attendance_credits += 3
         else:
-            self.attendance_credits = 0
-        return self.attendance_credits
+            attendance_credits = 0
+        return attendance_credits
     
     # NEWLY UPDATED
     # Calculate performance credits based on average score
-    def performance_credits(self, score):
+    def performance_credits_cal(self, score):
+        performance_credits = 0
         if score >= 85:
-            self.performance_credit_points += 5
+            performance_credits = 5
         elif score >= 60:
-            self.performance_credit_points += 3
+            performance_credits = 3
         else:
-            self.performance_credit_points = 0
-        return self.performance_credit_points
+            performance_credits = 0
+        return performance_credits
 
     # Calculate achievements (combines average score and session credits)
     def achievement_status(self):
+        final_credits = 0
         # we can make this score_credits(90) dynamic also, will see next
-        self.final_credits = self.performance_credits(90) + self.sessions_credits()
-        if self.final_credits >= 10:
+        final_credits = self.performance_credits_cal(90) + self.sessions_credits()
+        if final_credits >= 10:
             print("**** GOLD ****")
-        elif self.final_credits >= 8:
+        elif final_credits >= 8:
             print("**** SILVER ****")
         else:
             print("**** BRONZE ****")
     
     # Calculate trainer rating bonus 
     def trainer_ratings(self):
-        self.trainer_rating = int(input("Enter Trainer Rating: (1-5)"))
-        if self.trainer_rating >= 5:
+        trainer_rating = int(input("Enter Trainer Rating: (1-5)"))
+        if trainer_rating >= 5:
             return 5000
-        return 0    
+        else:
+            return 0    
--- lms-1/trainer.py
+++ lms-1/trainer.py
@@ -6,10 +6,6 @@ class Trainer:
         # Instance variables
         self.trainer_id = trainer_id
         self.trainer_name = trainer_name
-        self.total_sessions_taken = 0
-        self.payment_for_sessions = 0
-        self.training_bonus = 0
-        self.total_payment = 0
 
     # Non-static / Instance method: Display trainer info
     def trainer_info(self):
@@ -18,18 +14,16 @@ class Trainer:
         print(f"Trainer Name: {self.trainer_name}")
 
     # Non-static /Instance method: Calculate trainer payment (interactive)
-    def sessions_payment(self):
-        self.total_sessions_taken = int(input("Enter Total Sessions Taken: "))
+    def sessions_payment(self,student):
+        total_sessions_taken = int(input("Enter Total Sessions Taken: "))
         base_pay_per_session = 1500
-        self.payment_for_sessions = base_pay_per_session * self.total_sessions_taken
+        payment_for_sessions = base_pay_per_session * total_sessions_taken
         
         # Prompt for student's rating for the trainer (1 to 5):
         # If rating is 5 → add bonus of ₹5000
         # Otherwise → no bonus
-        s1 = Student()
-        print()
         print("=======Student Rating Given=======")
-        self.training_bonus = s1.trainer_ratings()
-        self.total_payment = self.payment_for_sessions + self.training_bonus
+        training_bonus = student.trainer_ratings()
+        total_payment = payment_for_sessions + training_bonus
         print("=======Trainer Payment=======")
-        print(f"Total Trainer Payment: {self.total_payment}")
+        print(f"Total Trainer Payment: {total_payment}")
